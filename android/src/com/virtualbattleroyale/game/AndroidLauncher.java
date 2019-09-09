8e2f2e8f935a9e21c539928e2f480b5eb0cfc83c
Artyom Abramovich <Artyom@convizit.com>
2019-09-09 12:34:03 +0300

    Initial Commit with all the starting files of the project.

---
+++ android/src/com/virtualbattleroyale/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.virtualbattleroyale.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.virtualbattleroyale.game.VirtualBattleRoyale;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new VirtualBattleRoyale(), config);
+	}
+}
