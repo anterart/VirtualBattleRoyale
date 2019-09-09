448e3f794653e9a2d7ff8ff5f05edd94cbcbf900
Artyom Abramovich <Artyom@convizit.com>
2019-09-09 12:57:22 +0300

    Example game finished.

--- core/src/com/virtualbattleroyale/game/VirtualBattleRoyale.java
+++ core/src/com/virtualbattleroyale/game/VirtualBattleRoyale.java
@@ -2,32 +2,46 @@ package com.virtualbattleroyale.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector3;
 
 public class VirtualBattleRoyale extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+	private OrthographicCamera cam;
+	private ShapeRenderer sr;
+	private Vector3 pos;
+
+	public void create(){
+		sr = new ShapeRenderer();
+		cam = new OrthographicCamera();
+		cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		pos = new Vector3(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0);
+
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render(){
+		//Logic
+		cam.update();
+
+		if(Gdx.input.isTouched()) {
+			pos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			cam.unproject(pos);
+		}
+
+		//Drawing
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		sr.begin(ShapeRenderer.ShapeType.Filled);
+		sr.setColor(Color.GREEN);
+		sr.circle(pos.x, pos.y, 64);
+		sr.end();
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+	public void dispose(){
+		sr.dispose();
 	}
 }
